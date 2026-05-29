babb2f6a3155f675b6dc7779f48be6ec373bf7f7
Hamd <nbhnnkmszq@privaterelay.appleid.com>
2026-05-29 16:03:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from flask import Flask, request, Response
 import requests
+import urllib3
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 app = Flask(__name__)
 
-# هذا الديكور يمسك أي مسار يبدأ بـ /loq/
-@app.route('/loq/<path:subpath>', methods=['GET', 'POST'])
-def proxy_to_snap(subpath):
-    target_url = f"https://app.snapchat.com/loq/{subpath}"
+@app.route('/proxy_login', methods=['POST'])
+def proxy_login():
+    # الرابط الأصلي اللي السناب يكلمه
+    target_url = "https://app.snapchat.com/loq/login"
     
-    # تحويل الطلب
-    resp = requests.request(
-        method=request.method,
-        url=target_url,
-        data=request.get_data(),
-        headers={k: v for k, v in request.headers if k.lower() != 'host'},
-        params=request.args,
-        verify=False
-    )
+    # 1. خذ البيانات (اليوزر والباسورد) اللي جات من السناب
+    raw_data = request.get_data()
+    
+    # 2. انسخ الهيدرات من السناب شات
+    headers = {k: v for k, v in request.headers if k.lower() != 'host'}
+    headers['Host'] = 'app.snapchat.com'
+    
+    # 3. مرر الطلب للسناب
+    resp = requests.post(target_url, data=raw_data, headers=headers, verify=False)
+    
+    # 4. أرجع الرد اللي يجي من السناب للتطبيق
     return Response(resp.content, status=resp.status_code, headers=dict(resp.headers))
 
 if __name__ == '__main__':
-    import os
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+    app.run(host='0.0.0.0', port=8080)
